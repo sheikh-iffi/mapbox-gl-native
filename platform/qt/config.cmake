471f6097b89d287e7fcb8863d2da39826cfc7724
Thiago Marcos P. Santos <tmpsantos@gmail.com>
2018-01-03 18:27:25 +0200

    [windows][build] Make tests optional
    Also do note use node.js or npm when tests are disabled.

--- platform/qt/config.cmake
+++ platform/qt/config.cmake
@@ -1,7 +1,9 @@
 include(platform/qt/qt.cmake)
 
-mason_use(sqlite VERSION 3.14.2)
-mason_use(gtest VERSION 1.8.0${MASON_CXXABI_SUFFIX})
+if(WITH_TESTS)
+    mason_use(sqlite VERSION 3.14.2)
+    mason_use(gtest VERSION 1.8.0${MASON_CXXABI_SUFFIX})
+endif()
 
 if(NOT WITH_QT_DECODERS)
     mason_use(libjpeg-turbo VERSION 1.5.0)
@@ -22,6 +24,8 @@ macro(mbgl_platform_core)
         PUBLIC platform/default
         PRIVATE platform/qt
         PRIVATE platform/qt/include
+        PRIVATE deps.old/tao/28626e9/include
+        PRIVATE deps.old/optional/5f08e2e/include
     )
 
     target_link_libraries(mbgl-core
@@ -62,32 +66,38 @@ macro(mbgl_filesource)
     target_link_libraries(mbgl-filesource
         ${MBGL_QT_FILESOURCE_LIBRARIES}
     )
-endmacro()
 
-
-macro(mbgl_platform_test)
-    target_sources(mbgl-test
-        PRIVATE platform/qt/test/main.cpp
-        PRIVATE platform/qt/test/qmapboxgl.test.cpp
-        PRIVATE platform/qt/test/qmapboxgl.test.cpp
-    )
-
-    target_include_directories(mbgl-test
-        PRIVATE platform/qt
-    )
-
-    set_source_files_properties(
-        platform/qt/test/main.cpp
-        PROPERTIES COMPILE_FLAGS -DWORK_DIRECTORY="${CMAKE_SOURCE_DIR}"
-    )
-
-    target_add_mason_package(mbgl-test PRIVATE sqlite)
-
-    target_link_libraries(mbgl-test
-        PRIVATE qmapboxgl
-        PRIVATE mbgl-filesource
+    target_include_directories(mbgl-filesource
+        PRIVATE deps.old/tao/28626e9/include
+        PRIVATE deps.old/optional/5f08e2e/include
     )
 endmacro()
+
+if(WITH_TESTS)
+    macro(mbgl_platform_test)
+        target_sources(mbgl-test
+            PRIVATE platform/qt/test/main.cpp
+            PRIVATE platform/qt/test/qmapboxgl.test.cpp
+            PRIVATE platform/qt/test/qmapboxgl.test.cpp
+        )
+
+        target_include_directories(mbgl-test
+            PRIVATE platform/qt
+        )
+
+        set_source_files_properties(
+            platform/qt/test/main.cpp
+            PROPERTIES COMPILE_FLAGS -DWORK_DIRECTORY="${CMAKE_SOURCE_DIR}"
+        )
+
+        target_add_mason_package(mbgl-test PRIVATE sqlite)
+
+        target_link_libraries(mbgl-test
+            PRIVATE qmapboxgl
+            PRIVATE mbgl-filesource
+        )
+    endmacro()
+endif()
 
 target_add_mason_package(qmapboxgl PRIVATE geojson)
 target_add_mason_package(qmapboxgl PRIVATE rapidjson)
